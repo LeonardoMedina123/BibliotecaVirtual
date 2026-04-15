5c5b292789e88da49ac6493e93f64ac6e7daace6
Leonardo Daniel Medina Castañeda <leonardomedina01234@gmail.com>
2026-04-15 11:34:28 -0600

    Subiendo el proyecto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="w-full py-4 px-6 bg-white shadow-sm">
+  <div class="max-w-6xl mx-auto flex items-center justify-between">
+    <div class="text-lg font-semibold">Biblioteca Virtual</div>
+    <nav class="space-x-4">
+      <a href="" class="text-slate-700 hover:text-slate-900">Login</a>
+      <a href="" class="text-slate-700 hover:text-slate-900">Registro</a>
+    </nav>
+  </div>
+</header>
+    hola mundo
+</body>
+</html>
